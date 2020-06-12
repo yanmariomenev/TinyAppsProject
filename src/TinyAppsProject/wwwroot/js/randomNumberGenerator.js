0aa0e210c25d46afe3108e5059e2fc4f9676a929
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2020-06-12 13:56:23 +0300

    add wwwroot

---
+++ src/TinyAppsProject/wwwroot/js/randomNumberGenerator.js
@@ -0,0 +1,10 @@
+﻿var button = document.getElementById("generate");
+var minimum = 1;
+var maximum = 1000;
+button.onclick = function generate() {
+    
+    var randomNum = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+    var el = document.getElementById('ranNum');
+    el.innerHTML = randomNum;
+}
+
